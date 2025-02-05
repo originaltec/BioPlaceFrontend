26b2afdc90c1280bfa61faa74088055870dbeda5
egor@originaltec.com <=>
2025-02-05 10:43:54 +0100

    Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/auth/sign-in/sign-in.component').then((c) => c.SignInComponent)
     },
     {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard.component').then((c) => c.DashboardComponent)
+    },
+    {
         path: '',
         redirectTo: 'sign-in',
         pathMatch: 'full'
