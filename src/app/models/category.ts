6affe9937ad1d37bb066b1e8439ed38b4b93d2bb
egor@originaltec.com <=>
2025-02-05 13:08:06 +0100

    Categories

--- src/app/models/category.ts
+++ src/app/models/category.ts
@@ -1,2 +1,46 @@
 export interface Category {
+  id: number;
+  name: string;
+  slug: string;
+  parent: number;
+  description: string;
+  display: string;
+  image: CategoryImage;
+  menuOrder: number;
+  count: number;
+  _links: Links;
+}
+
+export interface CategoryImage {
+  id: number;
+  dateCreated: string;
+  dateCreatedGmt: string;
+  dateModified: string;
+  dateModifiedGmt: string;
+  src: string;
+  name: string;
+  alt: string;
+}
+
+export interface Links {
+  self: Self[];
+  collection: Collection[];
+  up: Up[];
+}
+
+export interface Self {
+  href: string;
+  targetHints: TargetHints;
+}
+
+export interface TargetHints {
+  allow: string[];
+}
+
+export interface Collection {
+  href: string;
+}
+
+export interface Up {
+  href: string;
 }
--- src/app/pages/dashboard/categories/categories.component.ts
+++ src/app/pages/dashboard/categories/categories.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { Category } from '../../../models/product';
+import { MarketplaceService } from '../../../services/marketplace/marketplace.service';
+import { RouterModule } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
+
+   categories : Category [] = [];
+    errorMessage : string = '';
+  
+    constructor (private _marketPlaceService : MarketplaceService) {}
+  
+    ngOnInit() {
+      this.getCategories();
+    }
+  
+    getCategories () {
+  
+      this._marketPlaceService.getCategories().subscribe({
+  
+        next: (data) => {
+          console.log(data);
+          this.categories = data;
+        },
+  
+        error: (error) => {
+          this.errorMessage = error;
+        }
+  
+      });
+    }
 
 }
