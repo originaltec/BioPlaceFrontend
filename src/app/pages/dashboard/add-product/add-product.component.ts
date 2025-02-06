5018206670962fbbba0baff9e61bd0134c985e6f
egor@originaltec.com <=>
2025-02-06 13:11:01 +0100

    Select

--- src/app/pages/dashboard/add-product/add-product.component.ts
+++ src/app/pages/dashboard/add-product/add-product.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MarketplaceService } from '../../../services/marketplace/marketplace.service';
+import { Product } from '../../../models/product';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss'
 })
 export class AddProductComponent {
+
+  products : Product [] = [];
+  loading : boolean = true;
+
+  formControl !: FormControl;
+
+  constructor (private _marketPlaceService : MarketplaceService) {}
+
+  ngOnInit () {
+    this.getProducts();     
+  }
+
+  getProducts () {
+    this._marketPlaceService.getWooProducts().subscribe({
+
+      next: (data) => {
+        console.log(data);
+
+        this.products = data;
+        this.loading = false;
+      },
+
+      error: (error) => {
+        this.loading = false;
+      }
+
+    });
+  }
 
 }
