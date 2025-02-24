f2f8b7e8163c4f40dd16427066ef3a3979cf684b
egor@originaltec.com <=>
2025-02-24 12:36:58 +0100

    Que paso aqui

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,30 +1,50 @@
-import { CommonModule, NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { AuthServiceToken } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.scss'
+  styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
 
-  loginForm : FormGroup;
+  loginForm: FormGroup;
+  errorMessage: string = '';
 
-  constructor (private _formBuilder : FormBuilder) {
-
-    this.loginForm = this._formBuilder.group({
+  constructor(
+    private _authService : AuthServiceToken,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]]
     });
   }
 
-  login () {
-    console.log('send login');
+  login(): void {
+    
+    if (this.loginForm.invalid) {
+      return;
+    }
 
+    const { email, password } = this.loginForm.value;
+
+    this._authService.login(email, password).subscribe(
+      (response : any) => {
+        const token = response.token;
+        this._authService.saveToken(token);
+
+        console.log(token);
+      },
+      (error : any) => {
+        this.errorMessage = 'Credenciales incorrectas. Intenta de nuevo.';
+      }
+    );
   }
-
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthServiceToken {
 
-  constructor() { }
+  private apiUrl = 'https://localhost:7202'; 
+  private tokenKey = 'JWToken'; 
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    const loginPayload = { username, password };
+    return this.http.post<any>(`${this.apiUrl}/api/login`, loginPayload);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  removeToken(): void {
+    localStorage.removeItem(this.tokenKey);
+  }
+
+  isAuthenticated(): boolean {
+    const token = this.getToken();
+    return !!token;  
+  }
 }
